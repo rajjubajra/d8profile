9c2fd9687c002fa6403f849ed8691f528db00308
rajju <vajrachara@yahoo.com>
2019-09-20 03:14:06 +0100

    inital commit

--- contrib/bootstrap_sass/gulpfile.js
+++ contrib/bootstrap_sass/gulpfile.js
@@ -57,7 +57,7 @@ function js () {
 // Static Server + watching scss/html files
 function serve () {
   browserSync.init({
-    proxy: 'http://yourdomain.com',
+    proxy: 'http://yellow-website.com/profile',
   })
 
   gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
